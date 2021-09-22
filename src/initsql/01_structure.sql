4db957d21f9c175e0f0384a26d8860f0687d168b
Dario Salvi <dariosalvi78@gmail.com>
2021-09-22 14:03:32 +0200

    added structure for mobility and socio economic indicators

--- src/initsql/01_structure.sql
+++ src/initsql/01_structure.sql
@@ -55,3 +55,46 @@ ALTER TABLE public.epidemiology OWNER to syndemic;
 CREATE UNIQUE INDEX epidemiology_idx
 ON public.epidemiology (source, date, country_code, COALESCE(area1_code, ''), COALESCE(area2_code, ''), COALESCE(area3_code, ''));
 
+
+create table public.mobility (
+  "source" varchar NOT null,
+  "date" date NOT NULL,
+  "country_code" varchar(3) NOT NULL,
+  "area1_code" varchar null default null,
+  "area2_code" varchar null default null,
+  "area3_code" varchar null default null,
+  "gid" varchar NOT NULL,
+  "transit_stations" real null default null,
+  "residential" real null default null,
+  "workplace" real null default null,
+  "parks" real null default null,
+  "retail_recreation" real null default null,
+  "grocery_pharmacy" real null default null,
+  "transit" real null default null,
+  "walking" real null default null,
+  "driving" real null default null,
+  CONSTRAINT mobility_unique UNIQUE(source, date, country_code, area1_code, area2_code, area3_code)
+);
+ALTER TABLE public.mobility OWNER to syndemic;
+
+CREATE UNIQUE INDEX mobility_idx
+ON public.mobility (source, date, country_code, COALESCE(area1_code, ''), COALESCE(area2_code, ''), COALESCE(area3_code, ''));
+
+
+create table public.socio_economic (
+  "source" varchar NOT null,
+  "year" int NOT NULL,
+  "country_code" varchar(3) NOT NULL,
+  "area1_code" varchar null default null,
+  "area2_code" varchar null default null,
+  "area3_code" varchar null default null,
+  "gid" varchar NOT NULL,
+  "indicator" varchar not null,
+  "sample_size" int null default null,
+  "value" real null default null,
+  CONSTRAINT socio_economic_unique UNIQUE(source, year, country_code, area1_code, area2_code, area3_code)
+);
+ALTER TABLE public.socio_economic OWNER to syndemic;
+
+CREATE UNIQUE INDEX socio_economic_idx
+ON public.socio_economic (source, year, country_code, COALESCE(area1_code, ''), COALESCE(area2_code, ''), COALESCE(area3_code, ''));
