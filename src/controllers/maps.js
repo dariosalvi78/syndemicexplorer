9a214ac6fc727fa68b40bcd391ccbacc5c546b5c
Dario Salvi <dariosalvi78@gmail.com>
2021-09-30 11:19:06 +0200

    adding codes to api

--- src/controllers/maps.js
+++ src/controllers/maps.js
@@ -2,8 +2,9 @@ import {Pool} from '../db.js'
 
 
 export default {
+    // get country names and codes available in the maps
     async getCountryNames (req, res) {
-        let query = `select distinct country_name from admin_areas`
+        let query = `select distinct country_name, country_code from admin_areas`
         try {
             let data = await Pool.query(query)
             res.send(data.rows)
@@ -12,12 +13,14 @@ export default {
             res.sendStatus(500)
         }
     },
+    // get adm area 1 names and codes for one country
+    // country code must be specified as query param
     async getAdmArea_1_Names (req, res) {
         let countryCode = req.query.countryCode
         if (!countryCode) {
             res.sendStatus(404)
         } else {
-            let query = `select distinct area1_name from admin_areas where country_code = $1`
+            let query = `select distinct area1_name, area1_code from admin_areas where country_code = $1`
             try {
                 let data = await Pool.query(query, [countryCode])
                 res.send(data.rows)
