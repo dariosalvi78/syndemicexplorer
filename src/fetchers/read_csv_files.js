218a74fa8617b0c253e619c869a8a4c8845f5e48
Lgk2 <dunngen2@live.se>
2021-10-11 10:13:43 +0200

    The function now skips x bytes and stores the data as a java object

--- src/fetchers/read_csv_files.js
+++ src/fetchers/read_csv_files.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import readline from 'readline';
 
-function readCSVFiles(directoryName) {
+function readCSVFiles(directoryName, skipByteCount) {
     fs.readdir(directoryName, (err, filenames) => {
         if (err) {
             console.error(err);
@@ -14,7 +14,7 @@ function readCSVFiles(directoryName) {
                 var extension = splitFileName[splitFileName.length - 1];
 
                 if (extension === 'csv')
-                    readLines(directoryName + '/' + file);
+                    readLines(directoryName + '/' + file, skipByteCount);
             } catch (error) {
                 console.error(error);
             }
@@ -22,16 +22,37 @@ function readCSVFiles(directoryName) {
     }
 )}
 
-function readLines(file) {
+function readLines(file, skipByteCount) {
+    var rs = fs.createReadStream(file, {start: skipByteCount});
+
     const rl = readline.createInterface({
-        input: fs.createReadStream(file),
+        input: rs,
         output: process.stdout,
         terminal: false
     });
 
     rl.on('line', (line) => {
-        console.log(line);
+        line = line.split(';');
+
+        var obj = new DataObject(line[0], line[1], line[2]);
+
+        if (line.length == 5) {
+            obj.sample_size = line[3];
+            obj.value = line[4];
+        }
+        else
+            obj.value = line[3];
+
+        console.log(obj);
     });
 }
 
-readCSVFiles(process.cwd() + "/csv files");
+class DataObject {
+    constructor(year, area3, indicator) {
+        this.year = year;
+        this.area3 = area3;
+        this.indicator = indicator;
+    }
+}
+
+readCSVFiles(process.cwd() + "/csv files", 35);
