966bb818dba63f4096c7bcfe035e3ce898dcc4ec
Lgk2 <dunngen2@live.se>
2021-10-13 15:54:10 +0200

    Result is now stored in an array, worked on sql query for insertion

--- src/fetchers/read_csv_files.js
+++ src/fetchers/read_csv_files.js
@@ -1,25 +1,37 @@
 import fs from 'fs';
 import readline from 'readline';
 
+let csvFiles = -1;
+let result = new Array();
+
 function readCSVFiles(directoryName, skipByteCount) {
+    let allObjectsInFiles = new Array();
+
     fs.readdir(directoryName, (err, filenames) => {
         if (err) {
             console.error(err);
             return;
         }
 
+        let fileCount = 0;
         filenames.forEach(file => {
             try {
                 var extension = GetFileExtension(file);
 
-                if (extension === 'csv')
+                if (extension === 'csv') {
                     readLines(directoryName + '/' + file, skipByteCount);
+                    fileCount++;
+                }
             } catch (error) {
                 console.error(error);
             }
         });
-    }
-)}
+
+        csvFiles = fileCount;
+    })
+
+    return allObjectsInFiles;
+}
 
 function GetFileExtension(file) {
     var splitFileName = file.split('.');
@@ -30,6 +42,13 @@ function GetFileExtension(file) {
     return "null";
 }
 
+function StoreObjectsAndRunWhenFinished(objectCollection) {
+    result.push(objectCollection);
+
+    if (result.length == csvFiles) //All the files have been read
+        StoreDataInDB();
+}
+
 function readLines(file, skipByteCount) {
     var rs = fs.createReadStream(file, {start: skipByteCount});
 
@@ -38,6 +57,9 @@ function readLines(file, skipByteCount) {
         output: process.stdout,
         terminal: false
     });
+
+    var allobjects = new Array();
+    var index = 0;
 
     rl.on('line', (line) => {
         line = line.split(';');
@@ -51,8 +73,38 @@ function readLines(file, skipByteCount) {
         else
             obj.value = line[3];
 
-        console.log(obj);
+        allobjects[index] = obj;
+        index++;
     });
+
+    //All the lines in the file have been read and therefore the data is stored
+    rl.on('close', () => {
+        StoreObjectsAndRunWhenFinished(allobjects);
+    })
+}
+
+function StoreDataInDB() {
+    let createQuery = "CREATE TABLE demographics (source TEXT, year INTEGER, country_code TEXT, area1_code TEXT, area2_code TEXT, area3_code TEXT, Gid TEXT, indicator TEXT, sample_size INTEGER, value INTEGER);"
+
+    let query = "";
+
+    for (let file = 0; file < result.length; file++) {
+        for (let dataobject = 0; dataobject < result[file].length; dataobject++) {
+            let nbr = 1 + dataobject;
+            query += "INSERT INTO ??? VALUES (" + nbr + ", " + JSON.stringify(result[file][dataobject]) + ");" + "\n";
+        }
+    }
+
+    const client = new Pool({
+        user: "syndemic",
+          host: "localhost",
+          database: "syndemic",
+          password: "syndemic",
+          port: 5432,
+        })
+
+    console.log(createQuery);
+    console.log(query);
 }
 
 class DataObject {
