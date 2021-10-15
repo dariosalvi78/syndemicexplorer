ad3a240eeff81e31f99735e42645ef36d40d3fb2
JoelSven <62708808+JoelSven@users.noreply.github.com>
2021-10-15 14:31:27 +0200

    country dropdown

--- src/dashboard/dropDown.js
+++ src/dashboard/dropDown.js
@@ -1,42 +1,53 @@
-"use strict"
+"use strict";
 const dropdown1 = document.getElementById("level1");
 const buttonLevel1 = document.getElementById("level1Text");
-const buttonLevel2= document.getElementById("level2Text");
-          
-dropdown1.addEventListener('click', function(event) {
-    dropdown1.classList.toggle('is-active');
-    dropdown1.span
+const buttonLevel2 = document.getElementById("level2Text");
+
+dropdown1.addEventListener("click", function () {
+  dropDownContent1.innerHTML = "";
+  fillDropDown1();
+  dropdown1.classList.toggle("is-active");
 });
 
 const dropdown1Sweden = document.getElementById("sweden");
-dropdown1Sweden.addEventListener('click', function(event){
-    swedenLocation()
-    buttonLevel1.innerText = "Sweden";
+dropdown1Sweden.addEventListener("click", function (event) {
+  swedenLocation();
+  buttonLevel1.innerText = "Sweden";
 });
 
 const dropdown2 = document.getElementById("level2");
-dropdown2.addEventListener('click', function(event) {
-    
-    dropdown2.classList.toggle('is-active');
- });
+dropdown2.addEventListener("click", function (event) {
+  dropdown2.classList.toggle("is-active");
+});
 
- const dropDown2Skåne = document.getElementById("skåne");
- dropDown2Skåne.addEventListener('click',function(event){
-    stockholmLocation()
-    buttonLevel2.innerText = "Skåne";
- })
+const dropDown2Skåne = document.getElementById("skåne");
+dropDown2Skåne.addEventListener("click", function (event) {
+  stockholmLocation();
+  buttonLevel2.innerText = "Skåne";
+});
 
+const dropdown3 = document.getElementById("level3");
+dropdown3.addEventListener("click", function (event) {
+  dropdown3.classList.toggle("is-active");
+});
+const dropdown4 = document.getElementById("level4");
+dropdown4.addEventListener("click", function (event) {
+  dropdown4.classList.toggle("is-active");
+});
 
- 
-
- const dropdown3 = document.getElementById("level3");
- dropdown3.addEventListener('click', function(event) {
-     dropdown3.classList.toggle('is-active');
-  });
-  const dropdown4 = document.getElementById("level4");
-dropdown4.addEventListener('click', function(event) {
-    dropdown4.classList.toggle('is-active');
- });
- 
-
- 
+const dropDownContent1 = document.querySelector(".dropContent1");
+const urlCountry = "http://localhost:5000/api/v1/maps/countrynames";
+function fillDropDown1() {
+  fetch(urlCountry)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (myJson) {
+      console.log(myJson);
+      myJson.forEach(function (myJson) {
+        const level1 = document.createElement("a");
+        level1.innerHTML = myJson.country_name;
+        dropDownContent1.appendChild(level1);
+      });
+    });
+}
--- src/dashboard/mapbox.js
+++ src/dashboard/mapbox.js
@@ -1,36 +1,36 @@
 //import require from 'requirejs';
-//import mapboxgl from ''; 
+//import mapboxgl from '';
 //const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 //import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
- 
-mapboxgl.accessToken = 'pk.eyJ1Ijoiam9lbHN2ZW4iLCJhIjoiY2t1bWd3aXgwMWRrOTJxbzY1a3EwOTdhcyJ9.nrnAVPEsxD0zqcmH9g8E3g';
 
-const  successLocation =  position =>{
-    setupMap([position.coords.longitude, position.coords.latitude], 2)
-}
-const errorLocation = () =>{
-    setupMap([-5.00, 52.47], 2)
-}
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true})
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoiam9lbHN2ZW4iLCJhIjoiY2t1bWd3aXgwMWRrOTJxbzY1a3EwOTdhcyJ9.nrnAVPEsxD0zqcmH9g8E3g";
 
-const stockholmLocation = ()=> {
-    setupMap([17.56, 59,33], 7)
-}
-const swedenLocation = () =>{
-    setupMap([17.56, 59.33], 4)
-}
+const successLocation = (position) => {
+  setupMap([position.coords.longitude, position.coords.latitude], 2);
+};
+const errorLocation = () => {
+  setupMap([-5.0, 52.47], 2);
+};
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
+});
 
+const stockholmLocation = () => {
+  setupMap([17.56, 59, 33], 7);
+};
+const swedenLocation = () => {
+  setupMap([17.56, 59.33], 4);
+};
 
 const setupMap = (center, zoom) => {
-    const map = new mapboxgl.Map({
-        container: "map",
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: center,
-        zoom : zoom
-        });
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: zoom,
+  });
 
-    const nav = new mapboxgl.NavigationControl();
-    map.addControl(nav);
-}
-
-
+  const nav = new mapboxgl.NavigationControl();
+  map.addControl(nav);
+};
