ce543380eedf42bd494bff5f55ed4e79bd30cfd4
Lgk2 <dunngen2@live.se>
2021-10-20 14:30:10 +0200

    Revert "Added call to get boundingboxes"
    This reverts commit ac3d634bf4c8507cf75de3f2b92fb6c59bd6abc9.

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,8 @@ const router = express.Router()
 
 router.get('/maps/countrynames', mapsCtrl.getCountryNames)
 router.get('/maps/admarea1names', mapsCtrl.getAdmArea_1_Names)
-router.get('/maps/admareas1', mapsCtrl.getAdmAreas1)
+router.get('/maps/admareas1', mapsCtrl.getAdmAreas1) 
 router.get('/maps/admareas2', mapsCtrl.getAdmAreas2)
-router.get('/maps/admareas3', mapsCtrl.getAdmAreas3)
-router.get('/maps/boundingboxes', mapsCtrl.getBoundingBoxes)
+router.get('/maps/admareas3', mapsCtrl.getAdmAreas3) 
 
 export default router
