6429627d51db284f08f0d3696afd3cc5215254b4
Ardian Glamniki <ardian.glamniki@hotmail.com>
2021-10-20 19:37:22 +0200

    remove console.log

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,7 @@
 function queryStringToArray(item) {
-    console.log(item)
     item = item.replace('(', '')
     item = item.replace(')', '')
     var arr = item.split(',').map(Number)
-    console.log(arr)
     return arr;
 }
 export{queryStringToArray}
