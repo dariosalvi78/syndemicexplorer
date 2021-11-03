09b04e0031747efcc1c0d25a803339cb871aefda
Ardian Glamniki <ardian.glamniki@hotmail.com>
2021-11-03 12:11:08 +0100

    add some comments for the routes

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,23 @@ import mapsCtrl from './controllers/maps.js';
 
 const router = express.Router();
 
+// endpoint for listing the country names in the database
 router.get('/maps/countrynames', mapsCtrl.getCountryNames);
+
+// endpoint for listing the names of the regions of a country
+// countryCode has to be specified as query parameter
 router.get('/maps/admarea1names', mapsCtrl.getAdmArea_1_Names);
+
+// endpoint for regional data of a country 
+// countryCode has to be specified as query parameter
 router.get('/maps/admareas1', mapsCtrl.getAdmAreas1);
+
+// endpoint for municipal data of a region
+// area1Code has to be specified as query parameter
 router.get('/maps/admareas2', mapsCtrl.getAdmAreas2);
+
+// endpoint for city district data of a municipality
+// area2Code has to be specified as query parameter
 router.get('/maps/admareas3', mapsCtrl.getAdmAreas3);
 
 export default router;
