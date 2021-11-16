8c6c0fd86a4c7619b55003704f1f8551e74d7c59
Ardian Glamniki <ardian.glamniki@hotmail.com>
2021-11-16 17:33:49 +0100

    update fetchers

--- src/fetchers.js
+++ src/fetchers.js
@@ -1,7 +1,7 @@
 import fhm_kommun from './fetchers/fhm_kommun.js';
 
 let startFetchers = async function () {
-  await fhm_kommun();
+  fhm_kommun();
 };
 
-// export { startFetchers as startFetchers }
+ export { startFetchers as startFetchers }
