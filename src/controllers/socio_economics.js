ac026abf281dc23e4c89653e430915f22a41ed26
JoelSven <62708808+JoelSven@users.noreply.github.com>
2021-11-17 13:40:36 +0100

    clean up index.html

--- src/controllers/socio_economics.js
+++ src/controllers/socio_economics.js
@@ -2,64 +2,65 @@ import { Pool } from '../db.js';
 
 // TODO: add more indicators
 export default {
-    async getSocioEconomicIndicators(req, res) {
-        let query = `select * from socio_economic`
-        try {
-            let data = await Pool.query(query)
-            res.send(data.rows)
-        } catch (e) {
-            console.log(e)
-            res.sendStatus(500)
-        }
-    },
-    async getPopulation(req, res) {
-        let area3Code = req.query.area3Code
-        let year = req.query.year
-        let query = `select year, gid, indicator, value from socio_economic 
-        where indicator = 'demographics_age_total_all' and area3_code = $1 and year = `
-
-        if (!area3Code) {
-            res.sendStatus(400)
-          }
-
-        try {
-            let data
-            if(year) {
-                query += `$2`
-                data = await Pool.query(query, [area3Code, year])
-            } else {
-                query += `2020`
-                data = await Pool.query(query, [area3Code])
-            }
-            res.send(data.rows)
-        } catch (e) {
-            console.log(e)
-            res.sendStatus(500)
-        }
-    },
-    async getPopulationForeignBackground(req, res) {
-        let area3Code = req.query.area3Code
-        let year = req.query.year
-        let query = `select year, gid, indicator, value from socio_economic 
-        where indicator = 'foreign-background_total_all' and area3_code = $1 and year = `
-
-        if (!area3Code) {
-            res.sendStatus(400)
-          }
-
-        try {
-            let data
-            if(year) {
-                query += `$2`
-                data = await Pool.query(query, [area3Code, year])
-            } else {
-                query += `2020`
-                data = await Pool.query(query, [area3Code])
-            }
-            res.send(data.rows)
-        } catch (e) {
-            console.log(e)
-            res.sendStatus(500)
-        }
+  async getSocioEconomicIndicators(req, res) {
+    let query = `select * from socio_economic`;
+    try {
+      let data = await Pool.query(query);
+      res.send(data.rows);
+    } catch (e) {
+      console.log(e);
+      res.sendStatus(500);
     }
-}
+  },
+  //Jag ändrade lite här för att få med alla indicators för den area coden ------  where indicator = 'demographics_age_total_all' and
+  async getPopulation(req, res) {
+    let area3Code = req.query.area3Code;
+    let year = req.query.year;
+    let query = `select year, gid, indicator, value from socio_economic 
+        where area3_code = $1 and year = `;
+
+    if (!area3Code) {
+      res.sendStatus(400);
+    }
+
+    try {
+      let data;
+      if (year) {
+        query += `$2`;
+        data = await Pool.query(query, [area3Code, year]);
+      } else {
+        query += `2020`;
+        data = await Pool.query(query, [area3Code]);
+      }
+      res.send(data.rows);
+    } catch (e) {
+      console.log(e);
+      res.sendStatus(500);
+    }
+  },
+  async getPopulationForeignBackground(req, res) {
+    let area3Code = req.query.area3Code;
+    let year = req.query.year;
+    let query = `select year, gid, indicator, value from socio_economic 
+        where indicator = 'foreign-background_total_all' and area3_code = $1 and year = `;
+
+    if (!area3Code) {
+      res.sendStatus(400);
+    }
+
+    try {
+      let data;
+      if (year) {
+        query += `$2`;
+        data = await Pool.query(query, [area3Code, year]);
+      } else {
+        query += `2020`;
+        data = await Pool.query(query, [area3Code]);
+      }
+      res.send(data.rows);
+    } catch (e) {
+      console.log(e);
+      res.sendStatus(500);
+    }
+  },
+};
