c193c2b49a80fe7d9e93e33534145b1bfe08a1e8
Ardian Glamniki <ardian.glamniki@hotmail.com>
2021-12-01 15:47:11 +0100

    change query param to level instead of gid

--- src/controllers/heatmap.js
+++ src/controllers/heatmap.js
@@ -4,23 +4,23 @@ import { Pool } from '../db.js';
     let countryCode = req.query.countryCode;
     let indicator = req.query.indicator;
     let area1Code = req.query.area1Code;
-    let gid = req.query.gid;
+    let level = req.query.level;
     let query = ``;
 
     try {
       let data;
 
-      if(!countryCode && !indicator && !area1Code && !gid) {
+      if(!countryCode && !indicator && !area1Code && !level) {
         indicator = 'confirmed';
         query = getQuery(indicator, ' ');
         data = await Pool.query(query);
-      } else if (!countryCode || !indicator || !area1Code || !gid) {
+      } else if (!countryCode || !indicator || !area1Code || !level) {
           res.sendStatus(400);
           return;
       }
       else {
-        query = getQuery(indicator, 'where epidemiology.country_code = $1 and epidemiology.area1_code = $2 and epidemiology.gid = $3')
-        data = await Pool.query(query, [countryCode, area1Code, gid])
+        query = getQuery(indicator, 'where epidemiology.country_code = $1 and epidemiology.area1_code = $2 and admin_areas.level = $3')
+        data = await Pool.query(query, [countryCode, area1Code, level])
       }
       let features = [];
       for(let i in data.rows) {
